3eca45b2bd3756e2ad1c9c10513bf8016049a9dc
pgodettejr <109110310+pgodettejr@users.noreply.github.com>
2025-01-14 13:14:50 -0500

    Add recursive solution to Fibonacci sequence
    After many, many attempts, a solution was found on how to implement the
    Fibonacci sequence into an array using recursion.

--- fibonacci.js
+++ fibonacci.js
@@ -17,20 +17,38 @@ function fibs(n) {
 
 console.log(fibs(8));
 
-// TODO: How are we going to push the numbers bigger than 2 to the array. This won't cut it due to how the Fibonacci sequence works.
+// TODO: How are we going to push the numbers bigger than 'arr[1]' to the array? Previous 6 attempts didn't cut it due to how the Fibonacci sequence works.
+// Do we need a "for...in" loop or "forEach" method here instead? Due to the way array indexes are added (above: "i" doesn't LITERALLY equal the number, it just equals the position in the array)
+
+// I was missing several things: needed to return everything in every conditional after pushing them to the array, needed an 'else' statement that just returned the array[index] alone, needed to wrap all the conditionals inside a helper function then call the helper function inside the 'fibsRec' function before finally returning the full array. Was also missing the conditional 'arr.length <= i (NOT n)' because I always thought that 'arr.length' was always to the right of those expressions when I saw them used in loops elsewhere
+
+// It still doesn't quite work right because it prints '1' as the first index before going through the rest of the Fibonacci sequence
 function fibsRec(n) {
+  // console.log("This was printed recursively");
+
   let arr = [];
 
-  if (n < 2) {
-    arr.push(n);
-  } else {
-    n = fibsRec(n - 1) + fibsRec(n - 2);
-    arr.push(n);
+  // This performs the actual recursion in order to dodge redundant calcs found in non-optimized Fibonacci code
+  function recHelper(i) {
+    // Base case #1
+    if (i === 0) {
+      arr.push(0);
+      return 0;
+    // Base case #2
+    } else if (i === 1) {
+      arr.push(1);
+      return 1;
+    // Recursive case that computes Fibonacci(i) if it hasn't been computed already
+    } else if (arr.length <= i) {
+      let fib = recHelper(i - 1) + recHelper(i - 2);
+      arr.push(fib);
+      return fib;
+    } else {
+      return arr[i]; // Use the cached result if possible
+    }
+  }
 
-    // let fib = arr[fibsRec(n - 1)] + arr[fibsRec(n - 2)];
-    // arr.push(fib);
-  }  
-
+  recHelper(n); // Where you actually call the recursion with whatever value 'n' is
   return arr;
 }
 
