c8fb59203c7bd286befa0c82705206337fa5e619
pgodettejr <109110310+pgodettejr@users.noreply.github.com>
2025-01-27 15:22:03 -0500

    Refactored both functions and resolved all errors
    The merge function has been almost completely overhauled as the previous
    version was still a bit too close to the Java example that I was trying
    to base the function off of. The mergeSort function got the same kind
    of treatment but the changes there were far smaller.
    
    The empty 'data' array was removed along with the 'start' and 'end'
    variable declarations. The 'tempArray' was changed back to an empty
    array from the Array constructor that was there before (Java example).
    There are no more 'start, mid, end' parameters; just simple 'left' and
    'right' parameters representing split arrays themselves, not indexes
    within arrays fed to the merge function. The while loops were condensed
    down to 1 that deals with the actual merging of the sub-arrays while
    both still have values. Any remaining values left over on either array
    when the other is empty is dealt with in the 'return' statement using
    the 'concat' method. We also have the two indexes for each array
    declared as 0 at the top, simplifying from the 3 we had before (one was
    a 'tracker' of sorts for the tempArray). Deleted the 'for' loop as we
    no longer needed it for 'copying' elements to another array.
    
    The mergeSort basically stayed the same. The only major difference was
    using the 'slice' method for the actual splitting of the arrays instead
    of declaring multiple parameters in a conditional. The only conditional
    now is the base case. We also now use 'Math.floor' to calculate the
    midpoint of any array instead of trying to add two parameters together
    and dividing by 2.

--- mergeSort.js
+++ mergeSort.js
@@ -1,158 +1,59 @@
-// SIMPLE PSEUDOCODE
+// MERGE the selected arrays back together in sorted order
+function merge(left, right) {
+  // SET a temporary array to avoid modifying the contents of the original array when dealing with the sub-arrays
+  let tempArr = [];
 
-// READ the given/selected array
-// IF there's only one number
-  // RETURN the number
-// ELSE
-  // Sort left half (of the left half of the left half) of numbers
-  // Sort right half (of the right half of the right half) of numbers
-  // Merge sorted halves
-// ENDIF
+  // SET the index points for the "left" sub-array and the "right" sub-array
+  let i = 0;
+  let j = 0;
 
-
-// MORE COMPLEX PSEUDOCODE
-
-// READ the given/selected array
-// DIVIDE the selected array into two arrays by splitting it as evenly as possible
-  // OBTAIN the left sub-array
-  // IF there's only one number
-    // RETURN the number
-  // ELSE
-    // DIVIDE that sub-array into two arrays by splitting it as evenly as possible
-    // OBTAIN the left sub-array
-      // IF there's only one number
-        // RETURN the number
-      // ELSE
-        // DIVIDE that sub-array into two arrays by splitting it as evenly as possible
-        // WHEN an array of length 1 cannot be split, its ready for merge
-          // MERGE the selected arrays back together in sorted order
-            // GET the minimum of the two values
-            // ADD the selected value to the sorted array
-            // WHEN one list becomes empty 
-              // COPY all values from the remaining array into the sorted array
-              // ADD the selected value to the sorted array
-            // ENDMERGE
-          // WHEN an array of length 1 cannot be split, its ready for merge
-            // MERGE the selected arrays back together in sorted order
-            // SELECT the smallest value from the front of each list (excluded already sorted values)
-            // ADD the minimum of the two values to the sorted array
-            // REPEAT this process
-              // WHEN one list becomes empty 
-                // COPY all values from the remaining array into the sorted array
-                // ADD the selected value to the sorted array
-            // UNTIL there are no more values left to add to the sorted array
-          // ENDMERGE
-  // OBTAIN the right sub-array and REPEAT the above
-// MERGE the remaining selected arrays back together in sorted order
-  // SELECT the smallest value from the front of each list (excluded already sorted values)
-  // ADD the minimum of the two values to the sorted array
-  // REPEAT this process
-    // WHEN one list becomes empty 
-      // COPY all values from the remaining array into the sorted array
+  // WHILE both sub-arrays have values
+  while (i < left.length && j < right.length) {
+    // GET the minimum of the two values
+    if (left[i] < right[j]) {
       // ADD the selected value to the sorted array
-  // UNTIL there are no more values left to add to the sorted array
-// ENDMERGE
-
-
-// Example algo for Merge Sort
-
-// function merge(arr1, arr2, m, n) {
-//   let i = 0;
-//   let j = 0;
-//   let k = 0;
-//   let arr3 = []; // Array representing the previous two arrays merged together
-
-//   // Comparing and copying elements
-//   while (i <= m && j <= n) {
-//     if (arr1[i] < arr2[j]) {
-//       arr3[k++] = arr1[i++];
-//     } else {
-//       arr3[k++] = arr2[j++];
-//     }
-//   }
-
-//   // These 'for' loops copy any remaining elements to the third array (arr3)
-//   for ( ; i <= m; i++) {
-//     arr3[k++] = arr1[i];
-//   }
-
-//   for ( ; j <= n; j++) {
-//     arr3[k++] = arr2[j];
-//   }
-// }
-
-// function mergeSort(low, high) {
-//   if (low < high) {
-//     mid = (low + high) / 2;
-//     mergeSort(low, mid);
-//     mergeSort(mid + 1, high);
-//     merge(low, mid, high);
-//   }
-// }
-
-// let odinSort1 = [3, 2, 1, 13, 8, 5, 0, 1];
-// let odinSort2 = [105, 79, 100, 110];
-
-// console.log(mergeSort(odinSort1, l, h)); 
-// console.log(mergeSort(odinSort2, l, h));
-
-
-// Example #2 algo for Merge Sort (based on freeCodeCamp's Merge Sort recursion walkthrough)
-
-// Both arrays still show undefined whether or not this is active
-let data = [];
-
-// TODO: When we declare these, the code runs but we get 'undefined' arrays for both odinSort1 and odinSort2. Otherwise, we get the 'ReferenceError: start/end is not defined' on the console.logs
-let start;
-let end;
-
-function merge(data, start, mid, end) {
-  // Build a temp array to avoid modifying the original contents
-  let tempArr = new Array(end - start + 1);
-
-  let i = start;
-  let j = mid + 1;
-  let k = 0; // Why is k = 0 when i = start? Isn't 0 also start?
-
-  // While both sub-arrays have values, then try and merge them together in sorted order
-  while (i <= mid && j <= end) {
-    if (data[i] <= data[j]) {
-      tempArr[k++] = data[i++];
+      tempArr.push(left[i++]);
     } else {
-      tempArr[k++] = data[j++];
+      tempArr.push(right[j++]);
     }
   }
 
-  // Add the rest of the values from the left sub-array into the result
-  while (i <= mid) {
-    tempArr[k] = data[i];
-    k++;
-    i++;
-  }
-
-  // Add the rest of the values from the right sub-array into the result
-  while (j <= end) {
-    tempArr[k] = data[j];
-    k++;
-    j++;
-  }
-
-  for (i = start; i < end; i++) {
-    data[i] = tempArr[i - start];
-  }
+  // ADD any remaining values from either sub-array into the temporary array
+  return tempArr
+    .concat(left.slice(i))
+    .concat(right.slice(j));
 }
+// ENDMERGE
 
-function mergeSort(data, start, end) {
-  if (start < end) {
-    let mid = (start + end) / 2;
-    mergeSort(data, start, mid);
-    mergeSort(data, mid + 1, end);
-    merge(data, start, mid, end);
+// READ the given/selected array
+function mergeSort(arr) {
+  // IF the array only has a single value
+  if (arr.length === 1) {
+    // PRINT the array as its already sorted
+    return arr;
   }
+  // ENDIF
+
+  // DIVIDE the selected array into two sub-arrays by splitting it as evenly as possible
+  const mid = Math.floor(arr.length / 2); // SET the midpoint of the array before dividing it
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  
+  // OBTAIN the left sub-array, then...
+  return merge(
+    // CALL 'mergeSort' itself recursively using the midpoint instead of the last index as the final array item
+    mergeSort(left), 
+    // CALL 'mergeSort' itself recursively using the index after the midpoint instead of the first index as the first array item
+    mergeSort(right) 
+  );
 }
 
 let odinSort1 = [3, 2, 1, 13, 8, 5, 0, 1];
 let odinSort2 = [105, 79, 100, 110];
 
-console.log(mergeSort(odinSort1, start, end)); 
-console.log(mergeSort(odinSort2, start, end));
+console.log(mergeSort(odinSort1)); 
+console.log("Done!");
+
+console.log(mergeSort(odinSort2));
+console.log("Done!");
